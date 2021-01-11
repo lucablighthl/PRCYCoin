86fbdcb9e62b5e41d03297007277e147dd8439bf
lyricidal <lyricidal@gmail.com>
2021-01-11 11:06:25 -0500

    Adjust OptionsPage

--- src/qt/optionspage.cpp
+++ src/qt/optionspage.cpp
@@ -159,7 +159,8 @@ OptionsPage::OptionsPage(QWidget* parent) : QDialog(parent, Qt::WindowSystemMenu
     connect(ui->minimizeToTray, SIGNAL(stateChanged(int)), this, SLOT(minimizeToTray_clicked(int)));
     connect(ui->minimizeOnClose, SIGNAL(stateChanged(int)), this, SLOT(minimizeOnClose_clicked(int)));
     connect(ui->alwaysRequest2FA, SIGNAL(stateChanged(int)), this, SLOT(alwaysRequest2FA_clicked(int)));
-    connect(ui->hideBalance, SIGNAL(stateChanged(int)), this, SLOT(hideBalance_clicked(int)));
+    connect(ui->hideBalanceStaking, SIGNAL(stateChanged(int)), this, SLOT(hideBalanceStaking_clicked(int)));
+    connect(ui->lockSendStaking, SIGNAL(stateChanged(int)), this, SLOT(lockSendStaking_clicked(int)));
 }
 
 void OptionsPage::setStakingToggle()
@@ -1012,7 +1013,7 @@ void OptionsPage::alwaysRequest2FA_clicked(int state)
     }
 }
 
-void OptionsPage::hideBalance_clicked(int state) {
+void OptionsPage::hideBalanceStaking_clicked(int state) {
     int status = model->getEncryptionStatus();
     if (status == WalletModel::Locked || status == WalletModel::UnlockedForAnonymizationOnly) {
         QMessageBox msgBox;
@@ -1023,9 +1024,27 @@ void OptionsPage::hideBalance_clicked(int state) {
         msgBox.exec();
         return;
     }
-    if (ui->hideBalance->isChecked()) {
+    if (ui->hideBalanceStaking->isChecked()) {
         settings.setValue("fHideBalance", true);
     } else {
         settings.setValue("fHideBalance", false);
+    }
+}
+
+void OptionsPage::lockSendStakingclicked(int state) {
+    int status = model->getEncryptionStatus();
+    if (status == WalletModel::Locked || status == WalletModel::UnlockedForAnonymizationOnly) {
+        QMessageBox msgBox;
+        msgBox.setWindowTitle("Balance Settings");
+        msgBox.setIcon(QMessageBox::Information);
+        msgBox.setText("Please unlock the keychain wallet with your passphrase before attempting to change this setting.");
+        msgBox.setStyleSheet(GUIUtil::loadStyleSheet());
+        msgBox.exec();
+        return;
+    }
+    if (ui->lockSendStaking->isChecked()) {
+        settings.setValue("fLockSendStaking", true);
+    } else {
+        settings.setValue("fLockSendStaking", false);
     }
 }
--- src/qt/optionspage.h
+++ src/qt/optionspage.h
@@ -101,7 +101,8 @@ private Q_SLOTS:
     void minimizeOnClose_clicked(int);
     void changeDigits(int);
     void alwaysRequest2FA_clicked(int);
-    void hideBalance_clicked(int);
+    void hideBalanceStaking_clicked(int);
+    void lockSendStaking_clicked(int);
 };
 
 #endif // BITCOIN_QT_OPTIONSPAGE_H
