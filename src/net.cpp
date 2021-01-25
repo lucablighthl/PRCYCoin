d9e6be7f94b94db7dc971e3d3eb0894c8ad24b8f
lopeed <lopeed@hotmail.com>
2021-01-25 11:29:26 +0100

    Use ring buffer of set iterators instead of deque of copies in mruset

--- src/net.cpp
+++ src/net.cpp
@@ -2207,8 +2207,11 @@ unsigned int ReceiveFloodSize() { return 1000 * GetArg("-maxreceivebuffer", 5 * 
 
 unsigned int SendBufferSize() { return 1000 * GetArg("-maxsendbuffer", 1 * 1000); }
 
-CNode::CNode(SOCKET hSocketIn, CAddress addrIn, std::string addrNameIn, bool fInboundIn) : ssSend(SER_NETWORK, INIT_PROTO_VERSION), addrKnown(5000, 0.001, insecure_rand())
-
+CNode::CNode(SOCKET hSocketIn, CAddress addrIn, std::string addrNameIn, bool fInboundIn) :
+    ssSend(SER_NETWORK, INIT_PROTO_VERSION),
+    addrKnown(5000, 0.001, 0),
+    setInventoryKnown(SendBufferSize() / 1000)
+{
     nServices = 0;
     hSocket = hSocketIn;
     nRecvVersion = INIT_PROTO_VERSION;
@@ -2236,7 +2239,6 @@ CNode::CNode(SOCKET hSocketIn, CAddress addrIn, std::string addrNameIn, bool fIn
     nStartingHeight = -1;
     fGetAddr = false;
     fRelayTxes = false;
-    setInventoryKnown.max_size(SendBufferSize() / 1000);
     pfilter = new CBloomFilter();
     nPingNonceSent = 0;
     nPingUsecStart = 0;
